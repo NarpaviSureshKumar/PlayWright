2ccf9e8b896f48e909ac6257a200849864d1074c
NarpaviSureshKumar <101920352+NarpaviSureshKumar@users.noreply.github.com>
2024-11-30 19:59:21 +0530

    Add files via upload

---
+++ Week1Assignment.js
@@ -0,0 +1,18 @@
+// Week 1 Assignment
+
+
+let fullname = "Suresh Kumar S.";
+let companyName = "Amazon.";
+let mobileNumber = "9966216699.";
+let isAutomation = true;
+let playwright;
+
+console.log(`Name is `+ fullname + ` Data type of Variable is ` + typeof(fullname));
+
+console.log(`Company Name is `+ companyName + ` Data type of Variable is ` + typeof(companyName));
+
+console.log(`Mobile Number is `+ mobileNumber + ` Data type of Variable is ` + typeof(mobileNumber));
+
+console.log(`Automation is `+ isAutomation + ` Data type of Variable is ` + typeof(isAutomation));
+
+console.log(`Has Playwright `+ playwright + ` Data type of Variable is ` + typeof(playwright));
